97e647bb2c09f6a702902a87f6bcb53b8c57155d
juniorcesar001 <silvio_cbsj@hotmail.com>
2023-04-16 16:00:21 -0300

    Solução Desafio 4

--- secao2-dom/desafio-4/app.js
+++ secao2-dom/desafio-4/app.js
@@ -1,14 +1,41 @@
 new Vue({
 	el: '#desafio',
 	data: {
-
+		effect: false,
+		startEffect: false,
+		classe3: '',
+		style5: '',
+		progressWidth: 'width: 0%'
 	},
 	methods: {
 		iniciarEfeito() {
-
+			this.startEffect = true;
+			setInterval(() => {
+				this.effect = !this.effect;
+			}, 1000);
 		},
 		iniciarProgresso() {
+			let time = 0
+			const meuInterval = setInterval(() => {
 
+				this.progressWidth = 'width: ' + (time * 5) *  + "%";
+
+				time++
+
+				console.log(time)
+			}, 500);
+
+			setInterval(() => {
+				if (time >= 20)
+					clearInterval(meuInterval);
+			}, 500);
+		}
+	},
+	computed: {
+		efeito1() {
+			if(this.startEffect)
+				return this.effect 
+					? "destaque" : "encolher"
 		}
 	}
 })
